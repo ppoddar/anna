313ecfe58ea2ea4fc78bf1a65208296fee1bc989
ppoddar <pinaki.poddar@gmail.com>
2020-10-15 11:28:29 +0530

    dockerize

--- bin/deploy-docker.sh
+++ bin/deploy-docker.sh
@@ -11,17 +11,27 @@ GIT_USER=ppoddar
 GIT_REPO=anna.git
 WDIR=anna
 
+# -------------------------------
+# commit everything
 pushd $HOME_DIR > /dev/null
 git add -A 
 git commit -m "dockerize"
 git push origin master
 popd > /dev/null
+# -------------------------------
+# check out GIT REPO in a fresh directory
+# Build a docker image
+# run the image in host network. 
+# The database is running on host. So it can be reached via localhost
+# The node.js application is running at PORT 80 
 ssh -tt  -i $PEM $PROD_USER@$REMOTE_HOST << EOSSH
     rm -rf $WDIR
     git clone https://github.com/$GIT_USER/$GIT_REPO
     cd $WDIR
     docker build -t anna .
     docker run -d --network=host --rm anna 
+    sleep 2 
+    node src/populate_objects.js -d data/menu/
     exit 0
 EOSSH
-exit 0
+
